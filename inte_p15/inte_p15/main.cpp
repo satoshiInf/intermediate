061b9cff34f8f0e44b2550639f64d97add52bfff
Satoshi <informationcompetitionsatoshi@gmail.com>
2025-07-08 18:55:49 +0900

    add srand(time)

--- inte_p15/inte_p15/main.cpp
+++ inte_p15/inte_p15/main.cpp
@@ -4,7 +4,7 @@
 
  乱数を使って、ユーザーが「もう一度試すかどうか」を選べるCプログラムを作成してください。以下の条件をすべて満たしてください。
  ■条件：
- 乱数の生成には rand() を使用すること。
+ 乱数の生成には srand() に改修する
 
  最初に、使用する乱数の最大値（RAND_MAX）を printf を使って表示すること。
  乱数を1個生成し、画面に出力する。
@@ -18,24 +18,45 @@
  
  */
 
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int inputNum;
-    printf("乱数%dを生成する:\n ", RAND_MAX);
-    printf("%d,\n", rand() );
+    int input;
+    srand((unsigned int)time(NULL));
+    printf("生成する\n",RAND_MAX);
+    printf("生成されたものは%d\n",rand());
     
     do
     {
-        printf("1か0を入力: 1は再度入力、0は終了\n");
-        scanf("%d", &inputNum);
+        printf("入力する\n");
+        scanf("%d", &input);
     }
-    while (inputNum == 1 );
+    while (input == 1);
     
-    printf("0なので終了");
-    
+    printf("0は終了");
    
+    
     return 0;
 }
+
+
+/*
+int inputNum;
+
+srand(time(NULL));
+printf("乱数%dを生成する:\n ", RAND_MAX);
+printf("%d,\n", rand());
+
+do
+{
+    printf("1か0を入力: 1は再度入力、0は終了 \n");
+    scanf("%d", &inputNum);
+}
+while (inputNum == 1 );
+
+printf("0なので終了");
+
+*/
