b164296830da6bc65e4fe8b41b3ace83baad5427
Satoshi <informationcompetitionsatoshi@gmail.com>
2025-07-12 14:55:33 +0900

    add file

---
+++ inte_p56_forward_backward/inte_p56_forward_backward/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  inte_p56_forward_backward
+//
+//  Created by Satoshi on 7/12/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
